37a341e3ceb7351fb2d16a7d865577375eeaf0cf
misatol8k <misatolkurihara@gmail.com>
2021-01-13 23:12:28 +0900

    taskのCRUD機能を作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,40 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
-  
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    @task.update(task_params)
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.required(:task).permit(:name, :content, :id)
+  end
 end
