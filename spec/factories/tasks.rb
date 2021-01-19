d0cebe25a4075e1e5582e37bdd8e42da86c89847
misatol8k <misatolkurihara@gmail.com>
2021-01-19 22:49:32 +0900

    テストデータを変更

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,9 +2,11 @@ FactoryBot.define do
   factory :task do
     name { 'タスク1' }
     content { 'RSpecでテストを書く' }
+    end_date { '2021-01-16' }
   end
   factory :second_task, class: Task do
     name { 'タスク2' }
     content { 'テストを追加する' }
+    end_date { '2021-01-15' }
   end
 end
