c15a49f0db106210d113b32acc9b63f6c8de03ec
misatol8k <misatolkurihara@gmail.com>
2021-01-21 16:54:26 +0900

    検索のscopeをモデルに移動

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,7 @@ class Task < ApplicationRecord
   enum status:{
     未着手:0, 着手中:1,完了:2
   }
+  scope :search_name, -> (params){where('name LIKE ?', "%#{params}%")}
+  scope :search_status, -> (params){where(status: params)}
+  scope :sort_end_date, -> {order(end_date: :desc)}
 end
