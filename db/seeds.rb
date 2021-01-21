89fce0131c0bd780a2ba3d385336deb3f075fd23
misatol8k <misatolkurihara@gmail.com>
2021-01-22 02:20:55 +0900

    シードデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+Faker::Config.locale = :ja
+
+15.times do |n|
+  name = Faker::Book.title
+  content = Faker::Color.name
+  end_date = Faker::Date.in_date_period(year: 2021, month: 2)
+  Task.create!(name: name,
+              content: content,
+              end_date: end_date
+  )
+end
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
