80c61abb7f6b1ead5a7dcf65637f10303842423a
misatol8k <misatolkurihara@gmail.com>
2021-01-21 23:45:22 +0900

    優先順位のenum設定と検索scopeを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,13 @@
 class Task < ApplicationRecord
   validates :name, :content, presence: true
   enum status:{
-    未着手:0, 着手中:1,完了:2
+    未着手: 0, 着手中: 1,完了: 2
+  }
+  enum priority:{
+    低: 0, 中: 1, 高: 2
   }
   scope :search_name, -> (params){where('name LIKE ?', "%#{params}%")}
   scope :search_status, -> (params){where(status: params)}
   scope :sort_end_date, -> {order(end_date: :desc)}
+  scope :sort_priority, -> {order(priority: :desc)}
 end
