2342fc96de6385b81d928c6394ec5bd645075a33
misatol8k <misatolkurihara@gmail.com>
2021-01-25 23:01:35 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+  root 'sessions#new'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :tasks
   resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
