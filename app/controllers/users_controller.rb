afd04104138406a115e5a431aa72f845ee27d0d7
misatol8k <misatolkurihara@gmail.com>
2021-01-25 19:26:46 +0900

    userのCRUD機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,47 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def show
+  end
+
+  def update
+    if @user.update(user_params)
+      render :show, notice: "更新しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to new_session_path, notice: '削除しました！' }
+      format.json { head :no_content }
+    end
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :id)
   end
 end
