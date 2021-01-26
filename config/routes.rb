a6aee75dd36652b65cf23c20054924f062df4667
misatol8k <misatolkurihara@gmail.com>
2021-01-26 12:24:53 +0900

    adminのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
+
   root 'sessions#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :tasks
   resources :users
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
