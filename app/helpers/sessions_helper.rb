0f683af2890d4bf6dce711ed6388f9e87de8d744
misatol8k <misatolkurihara@gmail.com>
2021-01-26 12:07:33 +0900

    非ログイン時にログインページに遷移させるよう設定

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,8 +2,12 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
-  
+
   def logged_in?
     current_user.present?
   end
-end
+
+  def login_required
+    redirect_to new_session_path, notice: "ログインしてください" unless current_user
+    end
+  end
