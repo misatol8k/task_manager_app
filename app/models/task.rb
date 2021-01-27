952ca8930003598b0780e8c82cf1b963ce7b619e
misatol8k <misatolkurihara@gmail.com>
2021-01-27 11:42:50 +0900

    taskのselect指定をモデルのscopeに移動

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,6 +7,7 @@ class Task < ApplicationRecord
   enum priority:{
     低: 0, 中: 1, 高: 2
   }
+  scope :selected,  -> { select(:name, :content, :created_at, :end_date, :status, :priority, :id, :user_id) }
   scope :search_name, -> (params){where('name LIKE ?', "%#{params}%")}
   scope :search_status, -> (params){where(status: params)}
   scope :sort_end_date, -> {order(end_date: :desc)}
