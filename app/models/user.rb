5f33b9873d10f0a009dfc8a917e95025988fb54e
misatol8k <misatolkurihara@gmail.com>
2021-01-27 16:55:58 +0900

    adminが0人の場合、モデルのコールバックを利用して更新・削除をロールバックするよう設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
+  after_update :admin_exist
+  after_destroy :admin_exist
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
@@ -8,4 +10,12 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   scope :selected,  -> { select(:name, :email, :admin, :created_at, :updated_at, :id) }
 
+  private
+
+  def admin_exist
+    admin_count = User.where(admin: true).count
+    if admin_count <= 0
+      raise ActiveRecord::Rollback
+    end
+  end
 end
