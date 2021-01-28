93e86b7f44b5b3821865f95130499cf24e9446d8
misatol8k <misatolkurihara@gmail.com>
2021-01-28 12:46:47 +0900

    userのアソシエーションを設定、ログイン動作を追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,35 +1,42 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   before do
-    FactoryBot.create(:task)
-    FactoryBot.create(:second_task)
-    FactoryBot.create(:third_task)
-    visit tasks_path
+    @user_a = FactoryBot.create(:user)
+    FactoryBot.create(:task, user: @user_a)
+    FactoryBot.create(:second_task, user: @user_a)
+    FactoryBot.create(:third_task, user: @user_a)
+    visit new_session_path
+    fill_in 'Email', with: 'test1@test.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
   end
   describe '一覧表示機能' do
+    before do
+      visit tasks_path
+    end
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         expect(page).to have_content 'RSpecでテストを書く'
       end
     end
-  end
-  context 'タスクが作成日時の降順に並んでいる場合' do
-    it '新しいタスクが一番上に表示される' do
-      task_list = all('.task_row')
-      expect(task_list[0]).to have_content 'タスク2-2'
-      expect(task_list[1]).to have_content 'タスク2-1'
-      expect(task_list[2]).to have_content 'タスク1'
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content 'タスク2-2'
+        expect(task_list[1]).to have_content 'タスク2-1'
+        expect(task_list[2]).to have_content 'タスク1'
+      end
     end
-  end
-  context 'タスクを終了期限の降順でソートした場合' do
-    before do
-      click_link '終了期限▼'
-    end
-    it '終了期限の新しいタスクが一番上に表示される' do
-      task_list = all('.task_end_date')
-      expect(task_list[0]).to have_content '2021-01-16'
-      expect(task_list[1]).to have_content '2021-01-15'
-      expect(task_list[2]).to have_content '2021-01-14'
+    context 'タスクを終了期限の降順でソートした場合' do
+      before do
+        click_link '終了期限▼'
+      end
+      it '終了期限の新しいタスクが一番上に表示される' do
+        task_list = all('.task_end_date')
+        expect(task_list[0]).to have_content '2021-01-16'
+        expect(task_list[1]).to have_content '2021-01-15'
+        expect(task_list[2]).to have_content '2021-01-14'
+      end
     end
     context 'タスクを優先順位の高い順でソートした場合' do
       before do
@@ -44,6 +51,9 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
   end
   describe '検索機能' do
+    before do
+      visit tasks_path
+    end
     context 'タイトルであいまい検索をした場合' do
       it "検索キーワードを含むタスクで絞り込まれる" do
         fill_in 'タスク名', with: 'スク1'
@@ -77,6 +87,13 @@ RSpec.describe 'タスク管理機能', type: :system do
 end
 
 RSpec.describe 'タスク作成機能', type: :system do
+  before do
+    @user_a = FactoryBot.create(:user)
+    visit new_session_path
+    fill_in 'Email', with: 'test1@test.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Log in'
+  end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       before do
@@ -94,7 +111,7 @@ RSpec.describe 'タスク作成機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        before do
-         task = FactoryBot.create(:task)
+         task = FactoryBot.create(:task, user: @user_a)
          visit tasks_path
          click_link '詳細'
        end
