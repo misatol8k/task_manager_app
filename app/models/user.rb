81f0c6cd39ab7051645f90957bd5a54402020fe4
misatol8k <misatolkurihara@gmail.com>
2021-01-29 11:14:10 +0900

    管理者は自分自身を削除できない様に設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  has_many :tasks, dependent: :destroy
   after_update :admin_exist
+  before_destroy :admin_cannot_destroy_self
   after_destroy :admin_exist
+  has_many :tasks, dependent: :destroy
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
@@ -11,6 +12,9 @@ class User < ApplicationRecord
   scope :selected,  -> { select(:name, :email, :admin, :created_at, :updated_at, :id) }
 
   private
+  def admin_cannot_destroy_self
+    throw :abort if self.admin?
+  end
 
   def admin_exist
     admin_count = User.where(admin: true).count
