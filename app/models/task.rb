761a309fd6ef528f746181363174fabece074340
misatol8k <misatolkurihara@gmail.com>
2021-02-01 17:01:14 +0900

    scopeの条件文を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -10,8 +10,8 @@ class Task < ApplicationRecord
     低: 0, 中: 1, 高: 2
   }
   scope :selected,  -> { select(:name, :content, :created_at, :end_date, :status, :priority, :id, :user_id) }
-  scope :search_name, -> (params){where('name LIKE ?', "%#{params}%")}
-  scope :search_status, -> (params){where(status: params)}
+  scope :search_name, -> (params){where('name LIKE ?', "%#{params}%") if params.present? }
+  scope :search_status, -> (params){where(status: params) if params.present? }
   scope :sort_end_date, -> {order(end_date: :desc)}
   scope :sort_priority, -> {order(priority: :desc)}
 end
